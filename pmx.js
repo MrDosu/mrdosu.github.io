3a64aed8e22e5194ee3d1e5512fabc4ced0d8f9f
Sebastian Schreiber <sebastian.schreiber@jtl-software.com>
2019-02-22 14:43:48 +0100

    add test

--- pmx.js
+++ pmx.js
@@ -1,5 +1,17 @@
 var pmx = {
     test: function() {
         console.log("PMX is STILL working!");
+    },
+    globals: {
+        add: function(key, value) {
+            var current = pm.globals.get(key);
+            if(Array.isArray(current)) {
+                current.push(value);
+                pm.globals.set(key, current);
+            }
+            else {
+                pm.globals.set(key, [value]);
+            }
+        }
     }
 }
